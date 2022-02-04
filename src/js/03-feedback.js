db0bf1332b088e8a8979e7743d2fae601116f3e5
Rostyslav-Yefremov <rostislavefremow@gmail.com>
2022-02-04 15:11:36 +0200

    working 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const form = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
+const onLocal = evt => {
+  evt.preventDefault();
+
+  const formData = evt.currentTarget.elements;
+  const formInput = {
+    email: formData.email.value,
+    message: formData.message.value,
+  };
+
+  // const inputJSON = JSON.stringify(formInput);
+  // console.log(inputJSON);
+
+  const inputValue = localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formInput));
+  console.log('Данные формы', inputValue);
+
+  const valueLocal = localStorage.getItem(LOCAL_STORAGE_KEY);
+  console.log('Tut', valueLocal);
+};
+
+form.addEventListener('input', onLocal);
+
+const clearLocal = () => {
+  preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  console.log();
+};
+
+form.addEventListener('submit', clearLocal);
